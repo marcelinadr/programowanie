ba93c4566c50a4ffc3c8c2abf09b8a648498579b
Marcelina <marcelina@drozdzik.eu>
2025-11-26 12:16:53 +0100

    cwiczenie1

---
+++ cw1.sh
@@ -0,0 +1,34 @@
+#!usr/bin/bash
+declare a=2
+declare b=4
+declare c=7
+i=0
+echo "$a; $b; $c"
+for ((i=0; i<20; i++)); do 
+	a=$((b*i)); b=$((a+i)); c=$((a-i))
+	echo "$a; $b; $c"
+
+if [[ (a%2=0 && b%2=0 && c%2=0) || (i=20) ]]; then 
+	echo "$a; $b; $c"
+fi
+done 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
