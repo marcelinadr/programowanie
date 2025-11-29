6853ffe5df842a50e5c67ed11ebe58c3440f31b8
Marcelina <marcelina@drozdzik.eu>
2025-11-29 12:13:43 +0100

    cw2_1.sh

--- cw2_1.sh
+++ cw2_1.sh
@@ -1,9 +1,9 @@
 #!usr/bin/bash
-declare i=0
-declare j=0
+declare i=5
+declare j=8
 echo "Moje liczby: $i; $j"
 echo ""
-for ((i=5;i<65;i++)); do
+for ((i=$i;i<65;i++)); do
 	
         if (( i%2==0 )); then
 		               
